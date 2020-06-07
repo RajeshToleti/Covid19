9a2b3a5640246c347f96ec837d5d59a90b14db92
rajeshtoleti <rajeshtoleti@hotmail.com>
2020-06-07 18:06:38 +0100

    connected to API

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators, FormGroup } from "@angular/forms";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { environment } from "../../environments/environment";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
 import {
   MatSnackBar,
   MatSnackBarHorizontalPosition,
@@ -19,7 +25,7 @@ export class ContactComponent implements OnInit {
   emailFormControl: FormControl;
   commentFormControl: FormControl;
 
-  constructor(private _snackBar: MatSnackBar) {}
+  constructor(private _snackBar: MatSnackBar, private http: HttpClient) {}
 
   ngOnInit() {
     this.createFormControls();
@@ -49,9 +55,34 @@ export class ContactComponent implements OnInit {
     ) {
       return;
     }
-    console.log(this.nameFormControl.value);
-    console.log(this.emailFormControl.value);
-    console.log(this.commentFormControl.value);
+
+    //var formData: any = new FormData();
+    // formData.append("Name", this.nameFormControl.value);
+    // formData.append("Email", this.emailFormControl.value);
+    // formData.append("Comment", this.commentFormControl.value);
+    // console.log(JSON.stringify(formData));
+    const headers = new HttpHeaders().set("content-type", "application/json");
+    var body = {
+      name: this.nameFormControl.value,
+      Email: this.emailFormControl.value,
+      Comment: this.commentFormControl.value,
+    };
+    console.log(JSON.stringify(body));
+
+    // this.http.get("https://localhost:44368/api/Feedbacks").subscribe(
+    //   (response) => console.log(response),
+    //   (error) => console.log(error)
+    // );
+    this.http
+      .post("https://localhost:44368/api/Feedbacks", body, {
+        headers,
+      })
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+
+    //console.log(environment.ContactFormAPI);
 
     let message =
       this.nameFormControl.value + ", Thanks for your valuable feedback!";
@@ -64,6 +95,5 @@ export class ContactComponent implements OnInit {
     });
 
     this.contactForm.reset();
-    this.contactForm.untouched;
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  ContactFormAPI: "localhost:5456345/",
 };
